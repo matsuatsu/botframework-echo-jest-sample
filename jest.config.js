3e4a418f125997d246a4dbbaddf408ace5491e1e
Atsuki Matsuo <matsuo.de@gmail.com>
2023-08-13 22:46:27 +0900

    update

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots:["<rootDir>/src","<rootDir>/tests"],
-  moduleNameMapper:{
-    "^@/(.*)$": "<rootDir>/src/$1"
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
-  collectCoverage:true,
-  collectCoverageFrom:["./src/**/*.{ts,js}"]
+  collectCoverage: true,
+  collectCoverageFrom: ["./src/**/*.{ts,js}"],
 };
--- src/models/money.ts
+++ src/models/money.ts
@@ -1,6 +1,25 @@
 export class Money {
-    constructor(
-        public readonly amount:number,
-        public readonly currency:string,
-    ){}
+  constructor(
+    public readonly amount: number,
+    public readonly currency: string
+  ) {}
+
+  add(money: Money): Money {
+    if (this.currency == money.currency) {
+      return new Money(this.amount + money.amount, this.currency);
+    }
+  }
+
+  equal(money: Money): boolean {
+    const f1 = this.amount == money.amount;
+    const f2 = this.currency == money.currency;
+    if (f1 && f2) {
+      return true;
+    }
+    return false;
+  }
+
+  toString(): string {
+    return `${this.amount} ${this.currency}`;
+  }
 }
